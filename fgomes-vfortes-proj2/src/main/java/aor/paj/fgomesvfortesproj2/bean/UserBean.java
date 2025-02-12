74a949e93beeaaf947c0b272c8752b2ddfd427c0
Filipe Gomes <filipegomes@student.dei.uc.pt>
2025-02-12 17:31:55 +0000

    Remocao de codigo lixo

--- fgomes-vfortes-proj2/src/main/java/aor/paj/fgomesvfortesproj2/bean/UserBean.java
+++ fgomes-vfortes-proj2/src/main/java/aor/paj/fgomesvfortesproj2/bean/UserBean.java
@@ -21,146 +21,125 @@ import java.util.ArrayList;
 
 @ApplicationScoped
 public class UserBean implements Serializable {
-  private jakarta.inject.Provider<ServletContext> servletContext;
-  private static ArrayList<UserPojo> userPojos = new ArrayList<>();
+    private jakarta.inject.Provider<ServletContext> servletContext;
+    private static ArrayList<UserPojo> userPojos = new ArrayList<>();
 
-  public UserBean(){
-    loadUsersFromJson();
-  }
-
-  @Inject LoginBean loginBean;
-
-  public boolean login(String username, String password){
-    UserPojo u = UserBean.getUser(username,password);
-    if(u!= null){
-      loginBean.setCurrentUser(u);
-      return true;
-    }
-    else
-      return false;
-  }
-
-  public boolean register(String username, String password){
-    //TODO retirar
-    System.out.println("Registando utilizador: " + username);
-    System.out.println("password: " + password);
-    UserPojo u = UserBean.getUser(username, password);
-    if (u==null){
-      u= new UserPojo(username,password);
-      UserBean.addUser(u);
-      writeIntoJsonFile();
-      return true;
-    }else
-      return false;
-  }
-
-  public UserDto getLoggeduser(){
-    UserPojo u = loginBean.getCurrentUser();
-    if(u!= null)
-      return converUserPojoToUserDto(u);
-    else return null;
-  }
-
-  private UserDto converUserPojoToUserDto(UserPojo up){
-    UserDto ud = new UserDto(up.getUsername(), up.getPassword());
-    return ud;
-  }
-
- /* public void writeIntoJsonFile(ArrayList<UserDto> users) {
-    Jsonb jsonb = JsonbBuilder.create(new JsonbConfig().withFormatting(true));
-    String projectDir = System.getProperty("user.dir");  // Diretório atual do projeto
-    Path dataDir = Paths.get(projectDir, "data");  // Caminho completo para a pasta 'data'
-
-    try {
-      Files.createDirectories(filePath.getParent());
-      try (OutputStream os = Files.newOutputStream(filePath)) {
-        jsonb.toJson(users, os);
-        System.out.println("Arquivo JSON criado com sucesso");
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException("Erro ao escrever o arquivo JSON", e);
-    }
-  }*/
-
-
-  public void writeIntoJsonFile() {
-    // Cria o objeto Jsonb para serializar o objeto em JSON
-    Jsonb jsonb = JsonbBuilder.create();
-
-    // Defina o caminho da pasta 'data' dentro do seu projeto
-    String projectRoot = System.getProperty("user.home");
-    Path directoryPath = Path.of(projectRoot + File.separator + "data");
-
-    Path fPath = Path.of(directoryPath + File.separator + "utilizadores.json");
-    System.out.println("banana: " + fPath);
-    System.out.println(projectRoot);
-
-    /*String projectDir = System.getProperty("user.dir");  // Diretório atual do projeto
-    Path dataDir = Paths.get(projectDir, "data");  // Caminho completo para a pasta 'data'*/
-
-    // Crie a pasta 'data' caso ela não exista
-    try {
-      if (!Files.exists(directoryPath)) {
-        Files.createDirectories(directoryPath);  // Cria a pasta 'data'
-        System.out.println("Pasta 'data' criada com sucesso!");
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException("Erro ao criar a pasta 'data'", e);
+    public UserBean() {
+        loadUsersFromJson();
     }
 
-    /*// Defina o caminho do arquivo JSON dentro da pasta 'data'
-    Path filePath = dataDir.resolve("utilizadores.json");  // Caminho para o arquivo 'utilizadores.json'*/
+    @Inject
+    LoginBean loginBean;
 
-    // Escreva o arquivo JSON
-    try (OutputStream os = Files.newOutputStream(fPath)) {
-      jsonb.toJson(userPojos, os);  // Serializa o ArrayList<UserDto> em JSON e grava no arquivo
-      System.out.println("Arquivo JSON criado com sucesso em: " + fPath);
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException("Erro ao escrever o arquivo JSON", e);
+    public boolean login(String username, String password) {
+        UserPojo u = UserBean.getUser(username, password);
+        if (u != null) {
+            loginBean.setCurrentUser(u);
+            return true;
+        } else
+            return false;
     }
-  }
 
-
-  public static UserPojo getUser(String username, String password){
-    for(UserPojo u: userPojos){
-      if(u.getUsername().equals(username) && u.getPassword().equals(password))
-        return u;
+    public boolean register(String username, String password) {
+        //TODO retirar
+        System.out.println("Registando utilizador: " + username);
+        System.out.println("password: " + password);
+        UserPojo u = UserBean.getUser(username, password);
+        if (u == null) {
+            u = new UserPojo(username, password);
+            UserBean.addUser(u);
+            writeIntoJsonFile();
+            return true;
+        } else
+            return false;
     }
-    return null;
-  }
 
-  public static void addUser(UserPojo u) {
-    userPojos.add(u);
-  }
+    public UserDto getLoggeduser() {
+        UserPojo u = loginBean.getCurrentUser();
+        if (u != null)
+            return converUserPojoToUserDto(u);
+        else return null;
+    }
 
-  public void loadUsersFromJson() {
-    String projectRoot = System.getProperty("user.home");
-    Path directoryPath = Path.of(projectRoot + File.separator + "data");
+    private UserDto converUserPojoToUserDto(UserPojo up) {
+        UserDto ud = new UserDto(up.getUsername(), up.getPassword());
+        return ud;
+    }
 
-    Path filePath = Path.of(directoryPath + File.separator + "utilizadores.json");
 
-    System.out.println("A carregar ficheiro de utilizadores");
-
-    if (Files.exists(filePath)) {
-      try {
+    public void writeIntoJsonFile() {
+        // Cria o objeto Jsonb para serializar o objeto em JSON
         Jsonb jsonb = JsonbBuilder.create();
-        String content = Files.readString(filePath);
 
-        // Converte o JSON de volta para a lista de usuários
-        userPojos = jsonb.fromJson(content, new ArrayList<UserPojo>(){}.getClass().getGenericSuperclass());
+        // Defina o caminho da pasta 'data' dentro do seu projeto
+        String projectRoot = System.getProperty("user.home");
+        Path directoryPath = Path.of(projectRoot + File.separator + "data");
 
-        System.out.println("Usuários carregados do JSON com sucesso!");
+        Path fPath = Path.of(directoryPath + File.separator + "utilizadores.json");
+        System.out.println("banana: " + fPath);
+        System.out.println(projectRoot);
 
-        System.out.println(userPojos.toString());
 
-      } catch (IOException e) {
-        e.printStackTrace();
-        throw new RuntimeException("Erro ao ler o arquivo JSON", e);
-      }
+        // Crie a pasta 'data' caso ela não exista
+        try {
+            if (!Files.exists(directoryPath)) {
+                Files.createDirectories(directoryPath);  // Cria a pasta 'data'
+                System.out.println("Pasta 'data' criada com sucesso!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao criar a pasta 'data'", e);
+        }
+
+
+        // Escreva o arquivo JSON
+        try (OutputStream os = Files.newOutputStream(fPath)) {
+            jsonb.toJson(userPojos, os);  // Serializa o ArrayList<UserDto> em JSON e grava no arquivo
+            System.out.println("Arquivo JSON criado com sucesso em: " + fPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao escrever o arquivo JSON", e);
+        }
     }
-  }
+
+    public static UserPojo getUser(String username, String password) {
+        for (UserPojo u : userPojos) {
+            if (u.getUsername().equals(username) && u.getPassword().equals(password))
+                return u;
+        }
+        return null;
+    }
+
+    public static void addUser(UserPojo u) {
+        userPojos.add(u);
+    }
+
+    public void loadUsersFromJson() {
+        String projectRoot = System.getProperty("user.home");
+        Path directoryPath = Path.of(projectRoot + File.separator + "data");
+
+        Path filePath = Path.of(directoryPath + File.separator + "utilizadores.json");
+
+        System.out.println("A carregar ficheiro de utilizadores");
+
+        if (Files.exists(filePath)) {
+            try {
+                Jsonb jsonb = JsonbBuilder.create();
+                String content = Files.readString(filePath);
+
+                // Converte o JSON de volta para a lista de usuários
+                userPojos = jsonb.fromJson(content, new ArrayList<UserPojo>() {
+                }.getClass().getGenericSuperclass());
+
+                System.out.println("Usuários carregados do JSON com sucesso!");
+
+                System.out.println(userPojos.toString());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Erro ao ler o arquivo JSON", e);
+            }
+        }
+    }
 
 }
